0df38dab23065ddd24d3758d034c389c0e5730ec
Mark Courtney <immarkc@gmail.com>
2015-08-12 14:37:50 +0100

    Updates for Getter and Setter files with page content

--- SpringBasics/src/main/java/com/example/Application.java
+++ SpringBasics/src/main/java/com/example/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBasicsApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBasicsApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
